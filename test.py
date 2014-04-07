16c5a20e0a2f51416eeb4aaa36823298ef994c11
Adam Alpern <adam.alpern@gmail.com>
2014-04-06 23:30:30 -0700

    Test update

--- test.py
+++ test.py
@@ -1,19 +1,36 @@
 #!/usr/bin/env python
 
 import json
+import toolbox
 from cronenberg.model import *
+from cronenberg.data import Queries
 from cronenberg.cask.storage import EntityEncoder
+from toolbox.graphite.functions import *
+
+q = Queries(toolbox.PROD)
 
 dash = Dashboard(name='Automation Overview',
-                 queries={ 'events' : Query('events', targets=['foo.Count']),
-                           'bars' : Query('bars', targets=['bar.Count']) },
+                 queries={ 'api-rate' : Query('events', targets=[q.automation_api_rates()]),
+                           'api-latency' : Query('bars', targets=[q.automation_api_latency()]) },
                  grid=Grid(rows=[
                      [ GridEntry(span=3, presentation=SingleStatPresentation(title='Raw Events Processed',
-                                                                             query='events',
+                                                                             query='api-rate',
                                                                              attribute='sum')),
                        GridEntry(span=9, presentation=ChartPresentation(title="Foos",
-                                                                        query="bars",
+                                                                        query="api-latency",
                                                                         chart_type='timeseries')) ]
                  ]))
 
+
 print json.dumps(dash, cls=EntityEncoder, indent=4)
+#print json.dumps(target, cls=EntityEncoder, indent=4)
+
+#print vars(target)
+#print dir(target)
+
+#print str(target)
+#print vars(target)
+
+#print json.dumps(dash.to_json(), indent=4, sort_keys=True)
+
+#print group(1,1).to_json()
