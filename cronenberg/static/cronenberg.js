0b5354d0ef4be02d685acaadc42535cbec978017
Adam Alpern <adam.alpern@gmail.com>
2014-04-10 13:58:03 -0700

    Moved query handling from index.html to cronenberg.js

--- cronenberg/static/cronenberg.js
+++ cronenberg/static/cronenberg.js
@@ -1,5 +1,73 @@
 var cronenberg = {
 
+    queries: {},
+
+    /**
+     * TODO: should return a query object with chainable methods to
+     * register for events, etc...
+     */
+    add_query: function(name, url) {
+        console.log("cronenberg.add_query(): " + name + ' ' + url);
+        this.queries[name] = {
+            name: name,
+            url: url,
+            data: [],
+            presentations: []
+        };
+    },
+
+    _makeHandler: function(query) {
+        console.log("cronenberg._makeHandler()");
+        return function(data, textStatus) {
+            query.data = _.map(data, function(series) {
+                series.summation = cronenberg.reduce_series(series);
+                series.key = series.target;
+                series.values = series.datapoints;
+                console.log(series);
+                return series;
+                /*
+  WHY DOES THIS FAIL?
+              return {
+                    key: series.target,
+                    values: series.datapoints,
+                    summation: this.reduce_series(series)
+                };
+*/
+            });
+            bean.fire(query, 'data-available', query);
+        };
+    },
+
+
+    load_queries: function() {
+        console.log("cronenberg.load_queries()");
+        for (var query_name in this.queries) {
+            console.log('  load_queries(): ' + query_name);
+            var query = this.queries[query_name]
+            console.log(query);
+
+
+            // Notify any consumers of this query that it's reloading
+            bean.fire(query, 'loading');
+
+            $.ajax({
+                dataType: "json",
+                url: query.url
+            }).done(this._makeHandler(query));
+        }
+    },
+
+    activity: function(element) {
+        console.log("cronenberg.activity()");
+        /*
+        $(element).activity({
+            width: 4,
+            segments: 8,
+            length: 6,
+            space: 2
+        }); */
+    },
+
     id: function(entity) {
         var name = '';
         if ( typeof(entity) == "string" )
@@ -28,6 +96,7 @@ var cronenberg = {
      * the sum, min, max, mean, first, and last values.
      */
     reduce_series: function(series) {
+        console.log("cronenberg.reduce_series()");
         var sums = {
             sum: 0,
             min: Number.MAX_VALUE,
