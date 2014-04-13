b94dfb1919439b7913772c5b5a50d6a0d1290e82
Adam Alpern <adam.alpern@gmail.com>
2014-04-13 12:29:38 -0700

    GBC demo update

--- cronenberg/demo/gbc.py
+++ cronenberg/demo/gbc.py
@@ -9,34 +9,48 @@ def gbc_demo_dashboard():
                      category='Gooeybuttercake',
                      title='System Overview',
                      queries = {
-                         'cpu' : "group(aliasByNode(servers.s030[2-7].sysstat.cpu.all.system,1,4), aliasByNode(servers.s030[2-7].sysstat.cpu.all.user,1,4))",
+                         'cpu' : "group(aliasByNode(servers.s030[2-7].sysstat.cpu.all.system,1,5), aliasByNode(servers.s030[2-7].sysstat.cpu.all.user,1,5))",
                          'cpu_context_switches' : 'aliasByNode(servers.s030[2-7].sysstat.context_switches.context_switches,1)',
                          'tcp_sockets' : 'aliasByNode(servers.s030[2-7].sysstat.sockets.tcp,1)',
-                         'disk_reads' : 'aliasByNode(servers.s030[2-7].sysstat.io.blocks_read,1)',
+                         'disk_reads'  : 'aliasByNode(servers.s030[2-7].sysstat.io.blocks_read,1)',
                          'disk_writes' : 'aliasByNode(servers.s030[2-7].sysstat.io.blocks_written,1)'
                      },
                      grid=Grid(
-                         Row(Cell(span=12,
+                         Heading(text='CPU'),
+                         Row(Cell(span=10, offset=2,
                                   presentation=StandardTimeSeries(css_class='height4',
                                                                   title='CPU Usage',
                                                                   y_axis_label='CPU Usage %',
                                                                   query_name='cpu')))
                          ,Row(Cell(span=2,
-                                   presentation=SingleStat(title='Mean Context Switches',
+                                   presentation=[SingleStat(title='Mean Context Switches',
                                                            query_name='cpu_context_switches',
                                                            transform='mean',
                                                            units=' /sec',
-                                                           decimal=0))
+                                                           decimal=0)
+                                                 ,SingleStat(title='Max Context Switches',
+                                                             query_name='cpu_context_switches',
+                                                             transform='max',
+                                                             units=' /sec',
+                                                             decimal=0)
+                                             ]
+                               )
                              ,Cell(span=10,
                                    presentation=StackedAreaChart(css_class='height4',
                                                                  title='CPU Context Switches',
                                                                  y_axis_label='Context Switches per Second',
                                                                  query_name='cpu_context_switches')))
+                         ,Heading(text='Network')
                          ,Row(Cell(span=2,
-                                   presentation=SingleStat(title='Max TCP Sockets',
+                                   presentation=[SingleStat(title='Mean TCP Sockets',
                                                            query_name='tcp_sockets',
-                                                           transform='max',
-                                                           decimal=0))
+                                                           transform='mean',
+                                                           decimal=0)
+                                                 ,SingleStat(title='Max TCP Sockets',
+                                                             query_name='tcp_sockets',
+                                                             transform='max',
+                                                             decimal=0)
+                                                         ])
                              ,Cell(span=10,
                                    presentation=StackedAreaChart(css_class='height4',
                                                                  title='TCP Sockets',
