78dd9ed59071b0e4e45ef62952a07d2aad8ae9e2
Adam Alpern <adam.alpern@gmail.com>
2014-04-13 00:08:33 -0700

    from_time, until_time not needed during construction anymore

--- cronenberg/demo.py
+++ cronenberg/demo.py
@@ -196,35 +196,27 @@ class Queries(object):
 
 # =============================================================================
 
-def demo_dashboard(env, from_time, until_time):
+def demo_dashboard(env):
     q = Queries(env)
     graphite = env.graphite()
-
-    queries = {
-        'total_events_processed'   : q.total_events_processed(),
-        'total_triggers_processed' : q.total_triggers_processed(),
-        'total_triggers_satisfied' : q.total_triggers_satisfied(),
-        'total_pushes_sent'        : q.total_pushes_sent(),
-        'total_push_rate'          : q.total_push_rate(),
-        'end_to_end'               : q.automation_end_to_end_delivery_time(),
-        'immediate_triggers'       : q.immediate_triggers(),
-        'historical_triggers'      : q.historical_triggers(),
-        'api_rate'                 : q.automation_api_rates(),
-        'api_latency'              : q.automation_api_latency(),
-        'device_event_rate'        : q.device_event_rate(),
-        'stacked_test' : q.automation_push_payloads()
-    }
-
-    for k,v in queries.items():
-        # TODO - inflection.underscore() the key, to ensure it's JS
-        # safe
-        query = GraphiteQuery(v, format=Graphite.Format.JSON, from_time=from_time, until_time=until_time)
-        queries[k] = str(graphite.render_url(query))
 
     dash = Dashboard(name='automation_overview',
                      category='Automation',
                      title='Overview',
-                     queries=queries,
+                     queries = {
+                         'total_events_processed'   : q.total_events_processed(),
+                         'total_triggers_processed' : q.total_triggers_processed(),
+                         'total_triggers_satisfied' : q.total_triggers_satisfied(),
+                         'total_pushes_sent'        : q.total_pushes_sent(),
+                         'total_push_rate'          : q.total_push_rate(),
+                         'end_to_end'               : q.automation_end_to_end_delivery_time(),
+                         'immediate_triggers'       : q.immediate_triggers(),
+                         'historical_triggers'      : q.historical_triggers(),
+                         'api_rate'                 : q.automation_api_rates(),
+                         'api_latency'              : q.automation_api_latency(),
+                         'device_event_rate'        : q.device_event_rate(),
+                         'stacked_test' : q.automation_push_payloads()
+                     },
                      grid=Grid(
                          Row(Cell(span=3, emphasize=True, align='center',
                                   presentation=SingleStat(title='Raw Events Processed',
@@ -248,7 +240,7 @@ def demo_dashboard(env, from_time, until_time):
                                                           transform='sum')))
                          ,Separator()
                          ,Row(Cell(span=4,
-                                   presentation=JumbotronSingleStat(css_class='height4',
+                                    presentation=JumbotronSingleStat(css_class='height4',
                                                                     title='Average Push Rate',
                                                                     query_name='total_push_rate',
                                                                     transform='mean',
