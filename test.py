fbb7c8462f2848350164e0f7e91f814147da795a
Adam Alpern <adam.alpern@gmail.com>
2014-04-13 00:11:45 -0700

    demo update. closer to having clean targets

--- test.py
+++ test.py
@@ -2,25 +2,10 @@
 
 import json
 import toolbox
-from cronenberg.model import *
-from cronenberg.data import Queries
+from cronenberg.demo import demo_dashboard
 from cronenberg.cask.storage import EntityEncoder
-from toolbox.graphite.functions import *
 from cronenberg.application import demo_dashboard
 
-q = Queries(toolbox.PROD)
-
-dash = demo_dashboard('-3h', None)
+dash = demo_dashboard(toolbox.PROD)
 
 print json.dumps(dash, cls=EntityEncoder, indent=4)
-#print json.dumps(target, cls=EntityEncoder, indent=4)
-
-#print vars(target)
-#print dir(target)
-
-#print str(target)
-#print vars(target)
-
-#print json.dumps(dash.to_json(), indent=4, sort_keys=True)
-
-#print group(1,1).to_json()
