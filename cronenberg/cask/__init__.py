1cc3d44116de8a82afb01431c5fd3ee827c51b68
Adam Alpern <adam.alpern@gmail.com>
2014-04-21 00:01:18 -0700

    Removing cask

--- cronenberg/cask/__init__.py
+++
@@ -1,7 +0,0 @@
-import json
-
-from .storage import Entity, NamedEntity, EntityEncoder, EntityStorageManager
-from .web import WebManagerAdapter
-
-def dumps(data):
-    return json.dumps(data, cls=EntityEncoder, indent=4)
