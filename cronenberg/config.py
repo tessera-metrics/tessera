e46008b19778dfe76b0537ed086bfc84eb69e639
Adam Alpern <adam.alpern@gmail.com>
2014-04-20 21:50:16 -0700

    Added 1st DB model and create_db command

--- cronenberg/config.py
+++ cronenberg/config.py
@@ -2,6 +2,7 @@ DEBUG             = True
 DEFAULT_FROM_TIME = '-3h'
 DASHBOARD_APPNAME = 'Cronenberg'
 DASHBOARD_DATADIR = 'data'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///cronenberg.db'
 ENVIRONMENT_NAME  = 'prod'
 GRAPHITE_URL      = 'http://graphite.prod.urbanairship.com'
 SERVER_ADDRESS    = '0.0.0.0'
