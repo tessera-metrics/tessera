6ce0e22f5416139ebb4d744b7e350303f1faf77e
Adam Alpern <adam.alpern@gmail.com>
2014-04-23 20:45:33 -0700

    Config cleanup

--- cronenberg/config.py
+++ cronenberg/config.py
@@ -1,9 +1,8 @@
-DEBUG             = True
-DEFAULT_FROM_TIME = '-3h'
-DASHBOARD_APPNAME = 'Cronenberg'
-DASHBOARD_DATADIR = 'data'
+DEBUG                   = True
+DEFAULT_FROM_TIME       = '-3h'
+DASHBOARD_APPNAME       = 'Cronenberg'
+DASHBOARD_DATADIR       = 'data'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///cronenberg.db'
-ENVIRONMENT_NAME  = 'prod'
-GRAPHITE_URL      = 'http://graphite.prod.urbanairship.com'
-SERVER_ADDRESS    = '0.0.0.0'
-SERVER_PORT       = 5000
+GRAPHITE_URL            = 'http://graphite.prod.urbanairship.com'
+SERVER_ADDRESS          = '0.0.0.0'
+SERVER_PORT             = 5000
