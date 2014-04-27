b2ac97e8e416b638b4db9350fa193e42aa008da3
Adam Alpern <adam.alpern@gmail.com>
2014-04-27 10:13:16 -0700

    Normalize theme CSS files

--- cronenberg/config.py
+++ cronenberg/config.py
@@ -1,7 +1,7 @@
 DEBUG                   = True
 DEFAULT_FROM_TIME       = '-3h'
+DEFAULT_THEME           = 'light'
 DASHBOARD_APPNAME       = 'Cronenberg'
-DASHBOARD_DATADIR       = 'data'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///cronenberg.db'
 GRAPHITE_URL            = 'http://graphite.prod.urbanairship.com'
 SERVER_ADDRESS          = '0.0.0.0'
