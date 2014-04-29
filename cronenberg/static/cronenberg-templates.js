111682ca60b307fb4ba1967b5026cb2dc935df9a
alpern <alpern@urbanairship.com>
2014-04-29 13:22:51 -0700

    Consistent indentation

--- cronenberg/static/cronenberg-templates.js
+++ cronenberg/static/cronenberg-templates.js
@@ -61,11 +61,11 @@ cronenberg.TemplateRegistry = function() {
             return item.css_class ? item.css_class : '';
         });
         Handlebars.registerHelper('container_class', function(item) {
-          switch (item.layout) {
+            switch (item.layout) {
             case 'fixed': return 'container';
             case 'fluid': return 'container-fluid';
             default: return '';
-          }
+            }
         });
         Handlebars.registerHelper('style_class', function(item) {
             if (item.style) {
