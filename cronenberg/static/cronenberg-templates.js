f8d8401b69c5a48c1cdea4e7fc4a9e2947c4bd81
Adam Alpern <adam.alpern@gmail.com>
2014-04-30 12:42:57 -0700

    Don't redundantly register global helpers
    All the non-template specific helpers were being repeated registered in
    register_presentation(). Move them out so that only happens once.

--- cronenberg/static/cronenberg-templates.js
+++ cronenberg/static/cronenberg-templates.js
@@ -33,62 +33,7 @@ cronenberg.TemplateRegistry = function() {
             return cronenberg.templates.presentation_registry[itemType]({ item: item });
         });
 
-        // Because we can't dynamically dispatch the block helpers
-        // registered above, register another helper that can do that
-        // for generic rendering.
-        Handlebars.registerHelper('item', function(item) {
-            return new Handlebars.SafeString(cronenberg.templates.render_presentation(item));
-        });
-        Handlebars.registerHelper('markdown', function(value) {
-            return new Handlebars.SafeString(markdown.toHTML(value));
-        });
-        Handlebars.registerHelper('moment', function(format, value) {
-            return moment(value).format(format);
-        });
-        Handlebars.registerHelper('format', function(format, value) {
-            return d3.format(format)(value);
-        });
-        Handlebars.registerHelper('height', function(item) {
-            return item.height ? 'ds-height' + item.height : '';
-        });
-        Handlebars.registerHelper('span', function(item) {
-            return item.span ? 'col-md-' + item.span : '';
-        });
-        Handlebars.registerHelper('offset', function(item) {
-            return item.offset ? 'col-md-offset-' + item.offset : '';
-        });
-        Handlebars.registerHelper('css_class', function(item) {
-            return item.css_class ? item.css_class : '';
-        });
-        Handlebars.registerHelper('container_class', function(item) {
-            switch (item.layout) {
-            case 'fixed': return 'container';
-            case 'fluid': return 'container-fluid';
-            default: return '';
-            }
-        });
-        Handlebars.registerHelper('style_class', function(item) {
-            if (item.style) {
-                switch (item.style) {
-                case 'well':
-                    return 'well';
-                case 'callout_neutral':
-                    return 'bs-callout bs-callout-neutral';
-                case 'callout_info':
-                    return 'bs-callout bs-callout-info';
-                case 'callout_success':
-                    return 'bs-callout bs-callout-success';
-                case 'callout_warning':
-                    return 'bs-callout bs-callout-warning';
-                case 'callout_danger':
-                    return 'bs-callout bs-callout-danger';
-                }
-            }  else {
-                return '';
-            }
-        });
-
-        return this;
+        return self;
     };
 
     /**
@@ -111,3 +56,58 @@ cronenberg.TemplateRegistry = function() {
 };
 
 cronenberg.templates = new cronenberg.TemplateRegistry();
+
+// Because we can't dynamically dispatch the block helpers
+// registered above, register another helper that can do that
+// for generic rendering.
+Handlebars.registerHelper('item', function(item) {
+  return new Handlebars.SafeString(cronenberg.templates.render_presentation(item));
+});
+Handlebars.registerHelper('markdown', function(value) {
+  return new Handlebars.SafeString(markdown.toHTML(value));
+});
+Handlebars.registerHelper('moment', function(format, value) {
+  return moment(value).format(format);
+});
+Handlebars.registerHelper('format', function(format, value) {
+  return d3.format(format)(value);
+});
+Handlebars.registerHelper('height', function(item) {
+  return item.height ? 'ds-height' + item.height : '';
+});
+Handlebars.registerHelper('span', function(item) {
+  return item.span ? 'col-md-' + item.span : '';
+});
+Handlebars.registerHelper('offset', function(item) {
+  return item.offset ? 'col-md-offset-' + item.offset : '';
+});
+Handlebars.registerHelper('css_class', function(item) {
+  return item.css_class ? item.css_class : '';
+});
+Handlebars.registerHelper('container_class', function(item) {
+  switch (item.layout) {
+    case 'fixed': return 'container';
+    case 'fluid': return 'container-fluid';
+    default: return '';
+  }
+});
+Handlebars.registerHelper('style_class', function(item) {
+  if (item.style) {
+    switch (item.style) {
+      case 'well':
+      return 'well';
+      case 'callout_neutral':
+      return 'bs-callout bs-callout-neutral';
+      case 'callout_info':
+      return 'bs-callout bs-callout-info';
+      case 'callout_success':
+      return 'bs-callout bs-callout-success';
+      case 'callout_warning':
+      return 'bs-callout bs-callout-warning';
+      case 'callout_danger':
+      return 'bs-callout bs-callout-danger';
+    }
+  }  else {
+    return '';
+  }
+});
