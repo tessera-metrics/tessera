1a50eb79a17dcce7340f157c69b045269526301a
Adam Alpern <adam.alpern@gmail.com>
2014-04-30 12:49:32 -0700

    Remove less for now

--- Gruntfile.js
+++ Gruntfile.js
@@ -70,7 +70,6 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-less');
 
   grunt.registerTask('default', ['concat']);
 }
