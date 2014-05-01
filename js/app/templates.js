da75afe725a052d59f50f7a15f2e123fb5c5c721
Adam Alpern <adam.alpern@gmail.com>
2014-04-30 17:36:32 -0700

    Split the rest of the templates into individual files

--- js/app/templates.js
+++ js/app/templates.js
@@ -49,6 +49,12 @@ cronenberg.TemplateRegistry = function() {
 
         template = ds.templates.models[item.item_type];
         if (template) {
+            if (template.dataHandler && item.query_name) {
+                cronenberg.queries.registry[item.query_name].available(function(query) {
+                    template.dataHandler(query, item);
+                });
+            }
+
             return template({item: item});
         }
         return "<p>Unknown item type <code>" + item.item_type + "</code></p>";
