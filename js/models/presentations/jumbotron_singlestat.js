7857ea6d5692df450e4b3bea2513c62700c1fac8
alpern <alpern@urbanairship.com>
2014-05-02 13:47:09 -0700

    Fix jumbotron_singlestat

--- js/models/presentations/jumbotron_singlestat.js
+++ js/models/presentations/jumbotron_singlestat.js
@@ -7,9 +7,11 @@ ds.models.jumbotron_singlestat = function(data) {
   base = ds.models.singlestat(data).set_type('jumbotron_singlestat');
 
   d3.rebind(self, base,
+            'set_interactive',
             'set_css_class', 'set_element_id', 'set_height', 'set_style',
             'set_title', 'set_units', 'set_format', 'set_index', 'set_transform', 'toJSON');
   ds.rebind_properties(self, base,
+                       'item_type', 'css_class', 'element_id', 'height', 'style',
                        'query_name', 'title', 'units', 'format', 'index', 'transform');
 
   return self;
