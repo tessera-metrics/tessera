ce32ba2a66b1dce345f4281f2d3f7983f83ba68b
Adam Alpern <adam.alpern@gmail.com>
2014-05-03 12:40:13 -0700

    Added a toolbar button to change the grid layout

---
+++ js/app/menu-rejigger.js
@@ -0,0 +1,13 @@
+cronenberg.dashboards.onDashboardLoaded(function(d) {
+  $("ul.ds-rejigger-menu li").on('click', 'a', function(e) {
+    var target = $(e.target).parent();
+    var cols = target.attr('data-ds-cols');
+    var section_type = target.attr('data-ds-section-type');
+    var layout = ds.models.layout.SimpleGrid({
+      section_type: section_type,
+      columns: cols
+    });
+
+    cronenberg.dashboards.change_layout(layout);
+  });
+});
