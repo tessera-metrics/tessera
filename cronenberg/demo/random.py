73ee015df1af0f1e71122c411cd1238c433f1fab
Adam Alpern <adam.alpern@gmail.com>
2014-05-04 15:43:04 -0700

    Rename query_name => query
    In preparation for allowing in-line query objects

--- cronenberg/demo/random.py
+++ cronenberg/demo/random.py
@@ -28,28 +28,28 @@ def demo_random_data_dashboard():
                                            Cell(span=4,
                                                 items=[
                                                     JumbotronSingleStat(title='Jumbotron Singlestat',
-                                                                        query_name='cpu_usage',
+                                                                        query='cpu_usage',
                                                                         units='frobs')
                                                 ])
                                            ,Cell(span=8, style=DashboardItem.Style.WELL,
-                                                 items=StackedAreaChart(query_name='cpu_usage', height=3, title="stacked_area_chart"))
+                                                 items=StackedAreaChart(query='cpu_usage', height=3, title="stacked_area_chart"))
                                        ])
                                        ,Row(items=[
                                            Cell(span=3,offset=4,align='center', style=DashboardItem.Style.WELL,
                                                 items=SingleStat(title='Max. Frob Density',
-                                                                 query_name='cpu_usage',
+                                                                 query='cpu_usage',
                                                                  transform='max',
                                                                  units='frobs/kg',
                                                                  format=',.0f'))
                                            ,Cell(span=2,align='center',style=DashboardItem.Style.WELL,
                                                  items=SingleStat(title='Average Rate',
-                                                                  query_name='cpu_usage',
+                                                                  query='cpu_usage',
                                                                   transform='mean',
                                                                   units='/sec',
                                                                   format=',.2f'))
                                            ,Cell(span=3, style=DashboardItem.Style.WELL,align='center',
                                                  items=SingleStat(title='Total Frobs',
-                                                                  query_name='cpu_usage',
+                                                                  query='cpu_usage',
                                                                   transform='sum',
                                                                   units='frobs',
                                                                   format=',.0f'))
@@ -61,7 +61,7 @@ def demo_random_data_dashboard():
                                        ,Row(items=[
                                            Cell(span=12,
                                                 items=StandardTimeSeries(height=4,
-                                                                         query_name='cluster',
+                                                                         query='cluster',
                                                                          options={
                                                                              'yAxisLabel' : 'frobs per second',
                                                                              'margin' : {
@@ -77,44 +77,44 @@ def demo_random_data_dashboard():
                                                                + "is a ``summation_table`` presentation linked to the same query as the "
                                                                + "``standard_time_series`` presentation displayed above."))
                                            ,Cell(span=4,
-                                                 items=SummationTable(query_name='cluster',
+                                                 items=SummationTable(query='cluster',
                                                                       format=',.4f'))
                                            ,Cell(span=4,
                                                  items=DonutChart(title='Distribution of Frobs',
-                                                                  query_name='cluster'))
+                                                                  query='cluster'))
                                        ])
                                        ,Heading(text="Summaries")
                                        ,Separator()
                                        ,Row(items=[
                                            Cell(span=2,
-                                                items=SingleStat(title='Max', transform='max', format=',.1f', units='things', query_name='thing1'))
+                                                items=SingleStat(title='Max', transform='max', format=',.1f', units='things', query='thing1'))
                                            ,Cell(span=2,
-                                                 items=SingleStat(title='Average', transform='mean', format=',.1f', units='things', query_name='thing1'))
+                                                 items=SingleStat(title='Average', transform='mean', format=',.1f', units='things', query='thing1'))
                                            ,Cell(span=8,
-                                                 items=SimpleTimeSeries(query_name='thing1'))
+                                                 items=SimpleTimeSeries(query='thing1'))
                                        ])
                                        ,Row(items=[
                                            Cell(span=2,
-                                                items=SingleStat(title='Max', transform='max', format=',.1f', units='things', query_name='thing2'))
+                                                items=SingleStat(title='Max', transform='max', format=',.1f', units='things', query='thing2'))
                                            ,Cell(span=2,
-                                                 items=SingleStat(title='Average', transform='mean', format=',.1f', units='things', query_name='thing2'))
+                                                 items=SingleStat(title='Average', transform='mean', format=',.1f', units='things', query='thing2'))
                                            ,Cell(span=8,
-                                                 items=SimpleTimeSeries(query_name='thing2'))
+                                                 items=SimpleTimeSeries(query='thing2'))
                                        ])
                                        ,Row(items=[
                                            Cell(span=2,
-                                                items=SingleStat(title='Max', transform='max', format=',.1f', units='things', query_name='thing3'))
+                                                items=SingleStat(title='Max', transform='max', format=',.1f', units='things', query='thing3'))
                                            ,Cell(span=2,
-                                                 items=SingleStat(title='Average', transform='mean', format=',.1f', units='things', query_name='thing3'))
+                                                 items=SingleStat(title='Average', transform='mean', format=',.1f', units='things', query='thing3'))
                                            ,Cell(span=8,
-                                                 items=SimpleTimeSeries(query_name='thing3'))
+                                                 items=SimpleTimeSeries(query='thing3'))
                                        ])
                                        ,Row(items=[
                                            Cell(span=2,
-                                                items=SingleStat(title='Max', transform='max', format=',.1f', units='things', query_name='thing4'))
+                                                items=SingleStat(title='Max', transform='max', format=',.1f', units='things', query='thing4'))
                                            ,Cell(span=2,
-                                                 items=SingleStat(title='Average', transform='mean', format=',.1f', units='things', query_name='thing4'))
+                                                 items=SingleStat(title='Average', transform='mean', format=',.1f', units='things', query='thing4'))
                                            ,Cell(span=8,
-                                                 items=SimpleTimeSeries(query_name='thing4'))
+                                                 items=SimpleTimeSeries(query='thing4'))
                                        ])
                                    ])))))
