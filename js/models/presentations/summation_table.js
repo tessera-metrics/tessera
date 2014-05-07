8b5178f75e37b43ed17296b9f9ea1d957af81bf0
Adam Alpern <adam.alpern@gmail.com>
2014-05-07 00:05:31 -0700

    Add a title to summation table

--- js/models/presentations/summation_table.js
+++ js/models/presentations/summation_table.js
@@ -2,17 +2,20 @@ ds.models.summation_table = function(data) {
   "use strict";
 
   var striped
+    , title
     , format = ',.3f'
     , base
     , self = {};
 
   if (data) {
     striped = data.striped !== false
+    title = data.title
     format = data.format || format;
   }
   base = ds.models.item(data).set_type('summation_table').rebind(self);
 
   Object.defineProperty(self, 'striped', {get: function() { return striped; }});
+  Object.defineProperty(self, 'title', {get: function() { return title; }});
   Object.defineProperty(self, 'format', {get: function() { return format; }});
   Object.defineProperty(self, 'requires_data', {value: true});
 
@@ -30,9 +33,16 @@ ds.models.summation_table = function(data) {
     return self;
   }
 
+  self.set_title = function(_) {
+    title = _;
+    return self;
+  }
+
   self.toJSON = function() {
     return base.toJSON({
-      format: format
+      format: format,
+      striped: striped,
+      title: title
     });
   }
 
