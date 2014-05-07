a1790837b5f7357fdd4e7563b78d32530236bfa8
alpern <alpern@urbanairship.com>
2014-05-07 13:42:44 -0700

    Tag editing

--- js/app/handlers/dashboard-toolbar.js
+++ js/app/handlers/dashboard-toolbar.js
@@ -57,8 +57,34 @@ $(document).ready(function() {
           $(this).html(markdown.toHTML(value))
         }
       })
+
+      $("#ds-info-panel-edit-tags").tagsManager({
+        hiddenTagListName: 'ds-info-panel-edit-taglist',
+        tagClass: 'badge badge-primary',
+        prefilled: ds.manager.current.dashboard.tags.map(function(tag) {
+                     return tag.name
+                   })
+      })
+
     }
   })
+
+  /**
+   * Handler for tag changes. This gets run each time a complete tag
+   * is added or removed.
+   */
+  $(document).on('change', '[name="ds-info-panel-edit-taglist"]', function(e) {
+    var tags = $('#ds-info-panel-edit-tags').tagsManager('tags');
+    ds.manager.current.dashboard.set_tags(tags);
+
+    // TODO: this snippet is repeated all over (above). Consolidate to
+    // an update_metadata() on ds.manager or somesuch (i.e. update w/o
+    // definition)
+    var dash = ds.manager.current.dashboard.toJSON()
+    delete dash.definition
+    ds.manager.update(dash)
+  })
+
 
 
   $(document).on('click', '#ds-toggle-interactive-button', function(e) {
