3f0cf45ae78a033529c7120e3db93250b45dc945
alpern <alpern@urbanairship.com>
2014-05-08 15:42:30 -0700

    Formatting

--- js/app/handlers/menu-presentation-actions.js
+++ js/app/handlers/menu-presentation-actions.js
@@ -1,19 +1,27 @@
 $(document).on('click', 'ul.ds-action-menu li', function(event) {
-          var presentation_id = $(this).parent().parent().parent().parent().parent()[0].id;
-          var item = ds.manager.current.dashboard.get_item(presentation_id);
-        console.log(item);
-          switch (this.getAttribute('data-ds-action')) {
-          case 'open-in-graphite':
-              var composer_url = ds.charts.composer_url(item, { showTitle: true });
-              window.open(composer_url);
-              break;
-          case 'export-png':
-              var image_url = ds.charts.chart_url(item, { showTitle: true });
-              window.open(image_url);
-              break;
-          case 'export-svg':
-              var image_url = ds.charts.chart_url(item, { showTitle: true, format: 'svg' });
-              window.open(image_url);
-              break;
-          }
+
+  var presentation_id = $(this).parent().parent().parent().parent().parent()[0].id;
+  var item = ds.manager.current.dashboard.get_item(presentation_id);
+
+  switch (this.getAttribute('data-ds-action')) {
+
+    case 'open-in-graphite': {
+      var composer_url = ds.charts.composer_url(item, { showTitle: true });
+      window.open(composer_url);
+      break;
+    }
+
+    case 'export-png': {
+      var image_url = ds.charts.chart_url(item, { showTitle: true });
+      window.open(image_url);
+      break;
+    }
+
+    case 'export-svg': {
+      var image_url = ds.charts.chart_url(item, { showTitle: true, format: 'svg' });
+      window.open(image_url);
+      break;
+    }
+
+  }
 });
