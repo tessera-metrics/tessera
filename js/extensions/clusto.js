b1e7e902e28a8dee26ddf077d9304aa3c92dc0fd
Adam Alpern <adam.alpern@gmail.com>
2014-05-09 00:17:40 -0700

    Fuck yeah clusto

---
+++ js/extensions/clusto.js
@@ -0,0 +1,26 @@
+var clusto = window.clusto || {}
+
+clusto.cache = {}
+
+clusto.get_role = function(role) {
+  if (clusto.cache[role]) {
+    return clusto.cache[role]
+  } else {
+    var url = 'http://clusto.prod.urbanairship.com:9998/ua/search/prod,' + role + '.json'
+    var result = ''
+    $.ajax({
+      url: url,
+      type: 'GET',
+      async: false,
+      success: function(data) {
+        result = data.join(',')
+      }
+    })
+    clusto.cache[role] = result
+    return result
+  }
+}
+
+Handlebars.registerHelper('clusto', function(role) {
+  return '{' + clusto.get_role(role) + '}'
+})
