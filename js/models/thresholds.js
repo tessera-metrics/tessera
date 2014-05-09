a1247a216e68b63ea45ed667f1b84b6482644d2e
Adam Alpern <adam.alpern@gmail.com>
2014-05-09 08:12:40 -0700

    Convert thresholds model object to observable

--- js/models/thresholds.js
+++ js/models/thresholds.js
@@ -1,43 +1,27 @@
 ds.models.thresholds = function(data) {
   "use strict";
 
-  var summation_type = 'mean'
-    , warning
-    , danger
-    , self = {};
+  var storage = {}
+    , self = {}
+
+  limivorous.observable(self, storage)
+            .property(self, 'summation_type', storage)
+            .property(self, 'warning', storage)
+            .property(self, 'danger', storage)
 
   if (data) {
-    summation_type = data.summation_type  || summation_type;
-    warning = data.warning;
-    danger = data.danger;
-  }
-
-  Object.defineProperty(self, 'summation_type', {get: function() { return summation_type; }});
-  Object.defineProperty(self, 'warning', {get: function() { return warning; }});
-  Object.defineProperty(self, 'danger', {get: function() { return danger; }});
-
-  self.set_summation_type = function(_) {
-    summation_type = _;
-    return self;
-  }
-
-  self.set_warning = function(_) {
-    warning = _;
-    return self;
-  }
-
-  self.set_danger = function(_) {
-    danger = _;
-    return self;
+    self.summation_type = data.summation_type  || 'mean'
+    self.warning = data.warning
+    self.danger = data.danger
   }
 
   self.toJSON = function() {
     return {
-      summation_type: summation_type,
-      warning: warning,
-      danger: danger
+      summation_type: self.summation_type,
+      warning: self.warning,
+      danger: self.danger
     }
   }
 
-  return self;
+  return self
 }
