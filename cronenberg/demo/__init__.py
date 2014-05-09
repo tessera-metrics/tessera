9e88f3dbd004e058c3047a030d4ef39c9d068e15
alpern <alpern@urbanairship.com>
2014-05-09 12:35:00 -0700

    More billboard demo boards

--- cronenberg/demo/__init__.py
+++ cronenberg/demo/__init__.py
@@ -3,4 +3,4 @@ from .automation2 import demo_automation_2
 from .random import *
 from .gallery import *
 from .singlenode import *
-from .carbon import demo_carbon_dashboard
+from .banners import *
