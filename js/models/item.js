7cf5af97573aea6cbdf6e7e86fe78e12f832abec
Adam Alpern <adam.alpern@gmail.com>
2014-05-10 09:43:47 -0700

    Convert base item model to observable

--- js/models/item.js
+++ js/models/item.js
@@ -1,47 +1,47 @@
 ds.models.item = function(data) {
   "use strict";
 
-  var item_type
-    , item_id
-    , query
-    , css_class
-    , height
-    , style
-    , thresholds
-    , interactive = false // TODO: hack
-    , parent
-    , dashboard
-    , self = {};
+  var parent
+    , storage = {}
+    , self = {}
+
+
+  limivorous.observable(self, storage)
+            .property('item_type')
+            .property('item_id')
+            .property('css_class')
+            .property('height')
+            .property('style')
+            .property('thresholds') // moving to Query
+            .property('interactive')
+            .property('dashboard')
+            .property('query', {
+              get: function(context) {
+                if (typeof(context.query) === 'string' && context.dashboard) {
+                  return context.dashboard.definition.queries[context.query]
+                } else {
+                  return context.query
+                }
+              }
+            })
+  Object.defineProperty(self, 'is_dashboard_item', {value: true});
 
   if (data) {
-    item_type = data.item_type;
-    item_id = data.item_id;
-    query = data.query || {};
-    css_class = data.css_class;
-    height = data.height;
-    style = data.style;
-    thresholds = ds.models.thresholds(data.thresholds)
+    self.item_type = data.item_type;
+    self.item_id = data.item_id;
+    self.query = data.query || {};
+    self.css_class = data.css_class;
+    self.height = data.height;
+    self.style = data.style;
+    self.thresholds = ds.models.thresholds(data.thresholds)
   }
 
-  Object.defineProperty(self, 'item_type', {get: function() { return item_type; }});
-  Object.defineProperty(self, 'item_id', {get: function() { return item_id; }});
-  Object.defineProperty(self, 'css_class', {get: function() { return css_class; }});
-  Object.defineProperty(self, 'height', {get: function() { return height; }});
-  Object.defineProperty(self, 'style', {get: function() { return style; }});
-  Object.defineProperty(self, 'thresholds', {get: function() { return thresholds; }});
-  Object.defineProperty(self, 'interactive', {get: function() { return interactive; }});
-  Object.defineProperty(self, 'dashboard', {get: function() { return dashboard; }});
-  Object.defineProperty(self, 'query', {get: function() {
-                                          if (typeof(query) === 'string' && self.dashboard) {
-                                            return self.dashboard.definition.queries[query]
-                                          } else {
-                                            return query
-                                          }
-                                        }})
+  // temporary alias
+  self.set_type = self.set_item_type
 
   self.rebind = function(target) {
     parent = target;
-    d3.rebind(target, self, 'set_type', 'set_query', 'set_css_class', 'set_item_id','set_height', 'set_style', 'set_thresholds', 'set_interactive', 'set_dashboard', 'render', 'flatten');
+    d3.rebind(target, self, 'set_type', 'set_item_type', 'set_query', 'set_css_class', 'set_item_id','set_height', 'set_style', 'set_thresholds', 'set_interactive', 'set_dashboard', 'render', 'flatten');
     ds.rebind_properties(target, self, 'item_type', 'query', 'css_class', 'item_id', 'height', 'style', 'thresholds', 'interactive', 'dashboard');
     Object.defineProperty(target, '_base', {value: self});
     Object.defineProperty(target, 'is_dashboard_item', {value: true});
@@ -53,16 +53,16 @@ ds.models.item = function(data) {
    */
 
   self.render = function() {
-    var template = ds.templates.models[item_type];
+    var template = ds.templates.models[self.item_type];
     if (template) {
-      if (template.dataHandler && query) {
+      if (template.dataHandler && self.query) {
         self.query.on_load(function(q) {
           template.dataHandler(q, parent);
         });
       }
       return template({item: parent});
     } else {
-      return "<p>Unknown item type <code>" + item_type + "</code></p>";
+      return "<p>Unknown item type <code>" + self.item_type + "</code></p>";
     }
   }
 
@@ -81,74 +81,25 @@ ds.models.item = function(data) {
     return items;
   }
 
-  /**
-   * Data mutators
-   */
-
-  self.set_type = function(_) {
-    item_type = _;
-    return self;
-  }
-
-  self.set_query = function(_) {
-    query = _;
-    return self;
-  }
-
-  self.set_css_class = function(_) {
-    css_class = _;
-    return self;
-  }
-
-  self.set_item_id = function(_) {
-    item_id = _;
-    return self;
-  }
-
-  self.set_height = function(_) {
-    height = _;
-    return self;
-  }
-
-  self.set_style = function(_) {
-    style = _;
-    return self;
-  }
-
-  self.set_thresholds = function(_) {
-    thresholds = _;
-    return self;
-  }
-
-  self.set_interactive = function(_) {
-    interactive = _;
-    return self;
-  }
-
-  self.set_dashboard = function(_) {
-    dashboard = _;
-    return self;
-  }
-
   self.toJSON = function(data_) {
-    var data = data_ || {};
-    data.item_type = item_type;
-    if (item_id)
-      data.item_id = item_id;
+    var data = data_ || {}
+    data.item_type = self.item_type
+    if (self.item_id)
+      data.item_id = self.item_id
     // TODO: return query.toJSON() when server side supports it
-    if (query)
-      data.query = typeof(query) === 'string' ? query : query.name;
-    if (css_class)
-      data.css_class = css_class;
-    if (height)
-      data.height = height;
-    if (style)
-      data.style = style;
-    if (thresholds) {
-      data.thresholds = thresholds.toJSON()
+    if (self.query)
+      data.query = typeof(self.query) === 'string' ? self.query : self.query.name
+    if (self.css_class)
+      data.css_class = self.css_class
+    if (self.height)
+      data.height = self.height
+    if (self.style)
+      data.style = self.style
+    if (self.thresholds) {
+      data.thresholds = self.thresholds.toJSON()
     }
-    return data;
+    return data
   }
 
-  return self;
+  return self
 }
