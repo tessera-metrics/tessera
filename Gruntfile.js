dee5af3c5b8ea99777c7b2d324a1ee7e7b22b352
Adam Alpern <adam.alpern@gmail.com>
2014-05-14 21:19:12 -0700

    Refactored how app modes work
    Major cleanup of mode switching
    - accomplished by ds.app.switch_to_mode() with a set of named modes
    - element visibility controlled by ``data-de-hide`` and
    ``data-de-show`` data attributes
    - only one mode active at a time
    - elements that are hidden or shown when entering a mode are restored
    to they previous state by a closure pushed onto a mode stack

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,7 +64,7 @@ module.exports = function(grunt) {
         },
         src: [
           'js/core.js',
-          'js/cronenberg.js',
+          'js/app.js',
           'js/app/manager.js',
           'js/app/helpers.js',
           'js/app/charts.js',
