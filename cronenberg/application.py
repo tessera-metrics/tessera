ab2e6ab2508847de6830319a24da27bbf3981847
alpern <alpern@urbanairship.com>
2014-05-27 15:03:04 -0700

    Kill the banner comment

--- cronenberg/application.py
+++ cronenberg/application.py
@@ -3,10 +3,6 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
-# =============================================================================
-# Setup
-# =============================================================================
-
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 app.secret_key = 'e688f6cb-fc11-65fa-c091-aba197c56c66'
