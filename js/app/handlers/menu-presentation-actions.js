15248aaf569d0833c55f753cb51f1d951011af8b
Adam Alpern <adam.alpern@gmail.com>
2014-05-27 20:35:14 -0700

    Split the presentations action menu
    - View definition & commands to change the chart type have moved to the
    edit menu

--- js/app/handlers/menu-presentation-actions.js
+++ js/app/handlers/menu-presentation-actions.js
@@ -44,38 +44,6 @@ $(document).on('click', 'ul.ds-action-menu li', function(event) {
       break;
     }
 
-    case 'set-chart-type-stacked-area-chart': {
-      item.set_item_type('stacked_area_chart')
-      ds.manager.update_item_view(item)
-      break;
-    }
-
-    case 'set-chart-type-standard-time-series': {
-      item.set_item_type('standard_time_series')
-      ds.manager.update_item_view(item)
-      break;
-    }
-
-    case 'set-chart-type-simple-time-series': {
-      item.set_item_type('simple_time_series')
-      item.filled = false
-      ds.manager.update_item_view(item)
-      break;
-    }
-
-    case 'set-chart-type-simple-time-series-filled': {
-      item.set_item_type('simple_time_series')
-      item.filled = true
-      ds.manager.update_item_view(item)
-      break;
-    }
-
-    case 'view-definition': {
-      var contents = ds.templates.edit.item_source({item:item})
-      bootbox.alert(contents)
-      break;
-    }
-
   }
   /* prevents resetting scroll position */
   return false
