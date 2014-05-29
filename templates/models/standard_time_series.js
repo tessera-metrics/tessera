17dc81a97ffa752021f6781df709f8d1b878aedc
Adam Alpern <adam.alpern@gmail.com>
2014-05-28 22:19:24 -0700

    Simplified chart rendering
    Refactored the graphite renderer to conform to the main charts renderer
    interface. This simplifies the dataHandler functions for each chart type

--- templates/models/standard_time_series.js
+++ templates/models/standard_time_series.js
@@ -1,13 +1,7 @@
 ds.templates.models.standard_time_series.dataHandler =
-    function(query, item) {
-        if (!item.interactive) {
-            var element = $('#' + item.item_id + ' .ds-graph-holder');
-            var png_url = ds.charts.graphite.chart_url(item, {
-                height: element.height(),
-                width: element.width()
-            });
-            element.html($('<img/>').attr('src', png_url).height(element.height()).width(element.width()));
-        } else {
-            var chart = ds.charts.standard_line_chart($("#" + item.item_id + ' .ds-graph-holder'), item, query);
-        }
-    };
+  function(query, item) {
+      var renderer = item.interactive
+          ? ds.charts
+          : ds.charts.graphite
+      renderer.standard_line_chart($('#' + item.item_id + ' .ds-graph-holder'), item, query)
+  }
