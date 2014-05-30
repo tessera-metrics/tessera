ccb07140ea03242ff91c10dfb18e59cb9e86909c
Adam Alpern <adam.alpern@gmail.com>
2014-05-30 08:13:56 -0700

    formatting

--- js/app/handlers/menu-presentation-actions.js
+++ js/app/handlers/menu-presentation-actions.js
@@ -1,26 +1,26 @@
 ds.actions.register('presentation-actions', [
   ds.models.action({
-    name: 'time-spans',
+    name:    'time-spans',
     display: 'View across time spans...',
-    icon: 'fa fa-clock-o',
+    icon:    'fa fa-clock-o',
     hide: ds.app.Mode.TRANSFORM,
     handler: function(action, item) {
       ds.manager.apply_transform(ds.models.transform.TimeSpans(), item)
     }
   }),
   ds.models.action({
-    name: 'time-shifts',
+    name:    'time-shifts',
     display: 'Time shift...',
-    icon: 'fa fa-clock-o',
+    icon:    'fa fa-clock-o',
     hide: ds.app.Mode.TRANSFORM,
     handler: function(action, item) {
       ds.manager.apply_transform(ds.models.transform.TimeShift(), item)
     }
   }),
   ds.models.action({
-    name: 'isolate',
+    name:    'isolate',
     display: 'Isolate...',
-    icon: 'fa fa-eye',
+    icon:    'fa fa-eye',
     hide: ds.app.Mode.TRANSFORM,
     handler: function(action, item) {
       ds.manager.apply_transform(ds.models.transform.Isolate(), item)
@@ -31,36 +31,36 @@ ds.actions.register('presentation-actions', [
     divider: true
   }),
   ds.models.action({
-    name: 'open-in-graphite',
+    name:    'open-in-graphite',
     display: 'Open in Graphite...',
-    icon: 'fa fa-bar-chart-o',
+    icon:    'fa fa-bar-chart-o',
     handler: function(action, item) {
       var composer_url = ds.charts.graphite.composer_url(item, { showTitle: true });
       window.open(composer_url);
     }
   }),
   ds.models.action({
-    name: 'export-png',
+    name:    'export-png',
     display: 'Export PNG...',
-    icon: 'fa fa-picture-o',
+    icon:    'fa fa-picture-o',
     handler: function(action, item) {
       var image_url = ds.charts.graphite.chart_url(item, { showTitle: true });
       window.open(image_url);
     }
   }),
   ds.models.action({
-    name: 'export-png',
+    name:    'export-png',
     display: 'Export SVG...',
-    icon: 'fa fa-code',
+    icon:    'fa fa-code',
     handler: function(action, item) {
       var image_url = ds.charts.graphite.chart_url(item, { showTitle: true, format: 'svg' });
       window.open(image_url);
     }
   }),
   ds.models.action({
-    name: 'export-csv',
+    name:    'export-csv',
     display: 'Export CSV...',
-    icon: 'fa fa-table',
+    icon:    'fa fa-table',
     handler: function(action, item) {
       var image_url = ds.charts.graphite.chart_url(item, { showTitle: true, format: 'csv' });
       window.open(image_url);
