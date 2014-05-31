26922aacc54214f54f17b61b9a8127d4b4184560
Adam Alpern <adam.alpern@gmail.com>
2014-05-30 21:36:48 -0700

    Clear the body before appending rows, so the table doesn't grow every time we refresh

--- templates/models/summation_table.js
+++ templates/models/summation_table.js
@@ -1,7 +1,8 @@
 ds.templates.models.summation_table.dataHandler
     = function(query, item) {
-        var body = $('#' + item.item_id + ' tbody');
+        var body = $('#' + item.item_id + ' tbody')
+        body.empty()
         query.data.forEach(function(series) {
-            body.append(ds.templates.models.summation_table_row({series:series, item:item}));
-        });
-    };
+            body.append(ds.templates.models.summation_table_row({series:series, item:item}))
+        })
+    }
