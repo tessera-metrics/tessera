de3e468e40d59abb0b7c7a593d29c8d081c02b7a
Adam Alpern <adam.alpern@gmail.com>
2014-06-04 22:39:34 -0700

    More property editors

---
+++ templates/edit/properties/striped.js
@@ -0,0 +1,14 @@
+ds.templates.edit.properties.striped.editHandler =
+    function(property, item) {
+        $('#' + item.item_id + property.name).editable({
+            type: 'checklist',
+            value: item.striped,
+            source: [
+                { value: true, text: "striped" }
+            ],
+            success: function(ignore, newValue) {
+                item.striped = newValue.length > 0
+                ds.manager.update_item_view(item)
+            }
+        })
+    }
