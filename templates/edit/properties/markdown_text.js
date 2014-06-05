721157b1d6b74b9a55aeaf133a9c0b33555d9183
alpern <alpern@urbanairship.com>
2014-06-04 17:09:28 -0700

    Get item_id set right everywhere

--- templates/edit/properties/markdown_text.js
+++ templates/edit/properties/markdown_text.js
@@ -3,7 +3,6 @@ ds.templates.edit.properties.markdown_text.editHandler =
         $('#' + item.item_id + property.name).editable({
             type: 'textarea',
             success: function(ignore, newValue) {
-              console.log('heading_text.update: ' + newValue)
                 item.text = newValue
                 ds.manager.update_item_view(item)
             }
