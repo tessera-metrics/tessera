c577e009afc405b07f91de0b18234ae01607dc2d
alpern <alpern@urbanairship.com>
2014-06-09 15:01:10 -0700

    Refactor more existing dashboard items

--- templates/models/stacked_area_chart.js
+++
@@ -1,4 +0,0 @@
-ds.templates.models.stacked_area_chart.dataHandler =
-    function(query, item) {
-        ds.charts.stacked_area_chart($('#' + item.item_id + ' .ds-graph-holder'), item, query)
-    }
--- templates/models/standard_time_series.js
+++
@@ -1,4 +0,0 @@
-ds.templates.models.standard_time_series.dataHandler =
-    function(query, item) {
-        ds.charts.standard_line_chart($('#' + item.item_id + ' .ds-graph-holder'), item, query)
-    }
