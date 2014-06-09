3b254e61390ba0189cc5949536762c36c7497100
Adam Alpern <adam.alpern@gmail.com>
2014-06-08 17:47:01 -0700

    Get rid of .hbs files entirely for properties

--- js/core.js
+++ js/core.js
@@ -2,6 +2,11 @@ var ds = window.ds || {}
 ds.models = ds.models || {}
 ds.models.data = ds.models.data || {}
 ds.models.transform = ds.models.transform || {}
+ds.edit = ds.edit || {}
+ds.edit.properties = ds.edit.properties || {}
+ds.templates = ds.templates || {}
+ds.templates.edit = ds.templates.edit || {}
+ds.templates.edit.properties = ds.templates.edit.properties || {}
 
 /**
  * Helper function to (potentially) render a template with a given
--- templates/edit/properties/markdown_text.js
+++ templates/edit/properties/markdown_text.js
@@ -1,11 +1,13 @@
-ds.templates.edit.properties.markdown_text.editHandler =
-    function(property, item) {
+ds.templates.edit.properties.markdown_text = {
+    template: Handlebars.compile('<span class="ds-source">{{item.text}}</span>'),
+    editHandler: function(property, item) {
         $('#' + item.item_id + property.name).editable({
-          type: 'textarea',
-          value: item.text || '',
-          success: function(ignore, newValue) {
-            item.text = newValue
-            ds.manager.update_item_view(item)
-          }
+            type: 'textarea',
+            value: item.text || '',
+            success: function(ignore, newValue) {
+                item.text = newValue
+                ds.manager.update_item_view(item)
+            }
         })
+    }
 }
