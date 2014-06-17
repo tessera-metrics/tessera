cc197d5a2cf05bf0f0cb1a15a527dafa462f3c53
Jeff Forcier <jeff@bitprophet.org>
2014-06-16 17:18:23 -0700

    Flesh out setup.py with public-facing info

--- setup.py
+++ setup.py
@@ -8,12 +8,35 @@ version = _locals['__version__']
 setup(
     name=name,
     version=version,
+    description='Powerful multipurpose dashboard server',
+    license='Apache',
+
+    author='Urban Airship',
+    url='https://github.com/urbanairship/tessera',
+
     packages=find_packages(),
-    # Ensure templates, etc get pulled into sdists
-    include_package_data=True,
+    include_package_data=True, # Ensure templates, etc get pulled into sdists
     install_requires=[
         x.strip()
         for x in open('requirements.txt').readlines()
         if x and not x.startswith('#')
+    ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: No Input/Output (Daemon)',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python',
+        'Topic :: System :: Monitoring',
+        'Topic :: System :: Systems Administration',
     ],
 )
