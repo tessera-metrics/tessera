e78b3f53150a5f1c170b860f8719e982cf1c6f9e
Jeff Forcier <jeff@bitprophet.org>
2014-06-20 13:46:44 -0700

    Fix state bleed, add fixture import test

--- integration/main.py
+++ integration/main.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-from spec import Spec, skip
+from spec import Spec, skip, eq_
 from invoke import run
 
 
@@ -14,10 +14,14 @@ class Integration(Spec):
         assert not os.path.exists(self.dbpath), msg.format(self.dbpath)
 
     def teardown(self):
+        from tessera.application import db
         # Teardown only runs if setup completed, so the below will not nuke
         # pre-existing dbs that cause setup's check to fail.
         if os.path.exists(self.dbpath):
             os.remove(self.dbpath)
+        # Ensure no cached session crap
+        db.session.close_all()
+
 
     def is_importable(self):
         import tessera
@@ -29,4 +33,15 @@ class Integration(Spec):
         from tessera.model.database import Dashboard
         # Make sure we can create and look at the DB
         db.create_all()
-        assert len(Dashboard.query.all()) == 0
+        eq_(len(Dashboard.query.all()), 0)
+
+    def can_import_fixtures(self):
+        from tessera.application import db
+        from tessera.importer.json import JsonImporter
+        from tessera.model.database import Dashboard
+        db.create_all()
+        path = os.path.abspath(os.path.join(
+            os.path.dirname(__file__), '..', 'demo', 'demo-gallery.json'
+        ))
+        JsonImporter.import_file(path)
+        eq_(len(Dashboard.query.all()), 1)
