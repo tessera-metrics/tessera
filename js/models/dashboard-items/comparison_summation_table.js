eb2c8db224e0942ea9db6e963ca5b26f1ff20b4d
Adam Alpern <adam.alpern@gmail.com>
2014-06-24 18:27:01 -0700

    Working comparison table! fuck yeah.
    Just needs some CSS magic for the diff color coding and converting the
    diff display to percentages.

--- js/models/dashboard-items/comparison_summation_table.js
+++ js/models/dashboard-items/comparison_summation_table.js
@@ -1,3 +1,14 @@
+/**
+ * This iteration of comparison_summation_table takes a single query
+ * and applies a timeShift() value to it. Another approach is to take
+ * two arbitrary queries and compare them (so it's not strictly a
+ * time-based comparison).
+ *
+ * This version is slightly simpler to implement, because we don't
+ * need to join on two asynchronously fetched queries.
+ *
+ * maybe this item should be renamed timeshift_summation_table
+ */
 ds.register_dashboard_item('comparison_summation_table', {
 
   constructor: function(data) {
@@ -7,18 +18,45 @@ ds.register_dashboard_item('comparison_summation_table', {
                          .property('striped', {init: false})
                          .property('format', {init: ',.3s'})
                          .property('title')
-                         .property('query2')
+                         .property('shift', {init: "1d"})
+                         .property('query_override')
                          .extend(ds.models.item, {item_type: 'comparison_summation_table'})
                          .build()
     Object.defineProperty(self, 'requires_data', {value: true})
+
+    function update_query() {
+      self.query_override = shift_query(self.query, self)
+    }
 
     if (data) {
       self.striped = data.striped !== false
       self.title = data.title
       self.format = data.format || self.format
-      self.query2 = data.query2
+      self.shift = data.shift || self.shift
     }
+    self.on('change:query', function(e) {
+      update_query()
+    }).on('change:dashboard', function(e) {
+      update_query()
+    })
     ds.models.item.init(self, data)
+
+    function shift_query(input, item) {
+      if (input && input.is_query) {
+        var targets = input.targets
+        if (!targets)
+          return undefined
+        var group = 'group(' + input.targets.join(',') + ')'
+        return ds.models.data.Query()
+               .set_name(item.item_id + '_shift_' + input.name)
+               .set_targets([
+                 group,
+                 'timeShift(' + group + ', \"' + item.shift + '\")'
+               ])
+      } else {
+        return undefined
+      }
+    }
 
     self.toJSON = function() {
       var data = ds.models.item.json(self)
@@ -28,8 +66,8 @@ ds.register_dashboard_item('comparison_summation_table', {
         data.striped = self.striped
       if (self.title)
         data.title = self.title
-      if (self.query2)
-        data.query2 = self.query2
+      if (self.shift)
+        data.shift = self.shift
       return data
     }
 
@@ -38,10 +76,15 @@ ds.register_dashboard_item('comparison_summation_table', {
 
   data_handler: function(query, item) {
     var body = $('#' + item.item_id + ' tbody')
-//    body.empty()
-//    query.data.forEach(function(series) {
-//      body.append(ds.templates.models.comparison_summation_table_row({series:series, item:item}))
-//    })
+    var now  = query.data[0].summation
+    var then = query.data[1].summation
+    var diff = ds.models.data.Summation(now).subtract(then)
+    body.append(ds.templates.models.comparison_summation_table_body({
+      now:  now,
+      then: then,
+      diff: diff,
+      item: item
+    }))
   },
 
   template: ds.templates.models.comparison_summation_table,
@@ -51,6 +94,7 @@ ds.register_dashboard_item('comparison_summation_table', {
     'format',
     'title',
     'query',
+    'shift',
     { id: 'css_class', category: 'base' }
   ]
 })
