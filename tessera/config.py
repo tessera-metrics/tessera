14d0860def60456541775a79e3b527e5ecb2b2f6
Adam Alpern <adam.alpern@gmail.com>
2014-07-10 22:52:31 -0700

    Configuration of auto refresh period
    - Adds configuration of the available periods, as well as the default
    - Adds an interactive setting to the preferences page

--- tessera/config.py
+++ tessera/config.py
@@ -9,11 +9,25 @@ SERVER_ADDRESS             = '0.0.0.0'
 SERVER_PORT                = 5000
 INTERACTIVE_CHARTS_DEFAULT = True
 INTERACTIVE_CHARTS_RENDERER = 'nvd3'
+
 DASHBOARD_RANGE_PICKER = [
-      ('Past Hour', '-1h'),
-      ('Past 3 Hrs', '-3h'),
+      ('Past Hour',   '-1h'),
+      ('Past 3 Hrs',  '-3h'),
       ('Past 12 Hrs', '-12h'),
-      ('Past Day', '-1d'),
-      ('Past Wk', '-1w'),
-      ('Past 2 Wks', '-2w'),
+      ('Past Day',    '-1d'),
+      ('Past Wk',     '-1w'),
+      ('Past 2 Wks',  '-2w'),
 ]
+
+DASHBOARD_REFRESH_INTERVALS = [
+    ('None',             0),
+    ('30 seconds',      30),
+    ('1 minute',        60),
+    ('2 minutes',   2 * 60),
+    ('5 minutes',   5 * 60),
+    ('10 minutes', 10 * 60),
+    ('30 minutes', 30 * 60),
+    ('1 hour',     60 * 60)
+]
+
+DEFAULT_REFRESH_INTERVAL = 60
