7e015c9bcafed1819ec9fdf44a23f8c156edbc8c
Adam Alpern <adam.alpern@gmail.com>
2014-07-18 09:29:14 -0700

    Use UTC, quantize to 15-minute intervals

--- js/app/handlers/range-picker.js
+++ js/app/handlers/range-picker.js
@@ -10,14 +10,17 @@ $(document).on('dp.change', '#ds-range-picker-until', function(e) {
 
 $(document).on('click', '.ds-recent-range-picker li, .ds-recent-range-picker a, .ds-custom-range-picker ul li, .ds-custom-range-picker ul li a', function(e) {
   var range = $(e.target).attr('data-ds-range')
-  console.log(range)
 
   if (range === 'custom') {
     $('.ds-recent-range-picker').hide()
     $('.ds-custom-range-picker').show()
 
-    var now = moment() // TODO: quantize to 15-min interval
-    $('#ds-range-picker-until').data("DateTimePicker").setDate(now)
+    var now          = moment().utc().startOf('minute')
+    now.minute( Math.round(now.minute() / 15) * 15) // quantize to 15-min interval
+    var until_picker = $('#ds-range-picker-until').data("DateTimePicker")
+    until_picker.setDate(now)
+    until_picker.setMaxDate(now.endOf('day'))
+
     // set to now -3 hours
     // $('#ds-range-picker-from').data("DateTimePicker").setDate(moment())
 
