9b560934fc31f7d3f4d2ef28aedc68c2a9ebdb5c
Adam Alpern <adam.alpern@gmail.com>
2014-07-17 19:37:30 -0700

    Filling in rendering and event handlers

--- js/app/handlers/range-picker.js
+++ js/app/handlers/range-picker.js
@@ -1,12 +1,28 @@
+$(document).on('dp.change', '#ds-range-picker-from', function(e) {
+  var from = $('#ds-range-picker-from').data("DateTimePicker").getDate()
+  console.log('set from time: ' + moment(from).fromNow())
+})
+
+$(document).on('dp.change', '#ds-range-picker-until', function(e) {
+  var until = $('#ds-range-picker-until').data("DateTimePicker").getDate()
+  console.log('set until time: ' + moment(until).fromNow())
+})
+
 $(document).on('click', '.ds-recent-range-picker li, .ds-recent-range-picker a, .ds-custom-range-picker ul li, .ds-custom-range-picker ul li a', function(e) {
   var range = $(e.target).attr('data-ds-range')
   console.log(range)
+
   if (range === 'custom') {
     $('.ds-recent-range-picker').hide()
     $('.ds-custom-range-picker').show()
+
+    // TODO - set initial values of from/until fields to something
+    // reasonable
+
   } else if (range === 'recent') {
     $('.ds-custom-range-picker').hide()
     $('.ds-recent-range-picker').show()
+
   } else {
     ds.manager.set_time_range(range, null)
   }
