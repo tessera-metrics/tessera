39b3b750fb2eb23a61b4b881871d2794b69b23cb
Adam Alpern <adam.alpern@gmail.com>
2014-07-20 10:44:16 -0700

    Handle all iterables in EntityEncoder

--- tessera/model/web.py
+++ tessera/model/web.py
@@ -2,20 +2,20 @@ import json
 
 class EntityEncoder(json.JSONEncoder):
     def default(self, obj):
-            # TODO - handle iterables
-        if isinstance(obj, list) or isinstance(obj, tuple):
+        try:
             return [ self.default(i) for i in obj ]
-        elif isinstance(obj, dict):
-            result = {}
-            for key, value in obj.items():
-                result[key] = self.default(value)
-            return result
-        elif hasattr(obj, 'to_json') and callable(getattr(obj, 'to_json')):
-            return obj.to_json()
-        elif hasattr(obj, '__dict__'):
-            return self.default(obj.__dict__)
-        else:
-            return obj
+        except TypeError, e:
+            if isinstance(obj, dict):
+                result = {}
+                for key, value in obj.items():
+                    result[key] = self.default(value)
+                return result
+            elif hasattr(obj, 'to_json') and callable(getattr(obj, 'to_json')):
+                return obj.to_json()
+            elif hasattr(obj, '__dict__'):
+                return self.default(obj.__dict__)
+            else:
+                return obj
 
 def dumps(obj):
     return json.dumps(obj, cls=EntityEncoder, indent=0)
