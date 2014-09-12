ef4a696a4663c3ec4af57a79f6a3b3ac703c77b3
Adam Alpern <alpern@urbanairship.com>
2014-09-12 14:49:36 -0700

    Add option to display series color in summation table

--- js/models/dashboard-items/summation_table.js
+++ js/models/dashboard-items/summation_table.js
@@ -9,16 +9,20 @@ ds.register_dashboard_item('summation_table', {
 
     var self = limivorous.observable()
                          .property('striped', {init: false})
+                         .property('show_color', {init: false})
                          .property('format', {init: ',.3s'})
                          .property('title')
+                         .property('options')
                          .extend(ds.models.item, {item_type: 'summation_table'})
                          .build()
     Object.defineProperty(self, 'requires_data', {value: true})
 
     if (data) {
-      self.striped = data.striped !== false
+      self.striped = Boolean(data.striped)
+      self.show_color = Boolean(data.show_color)
       self.title = data.title
       self.format = data.format || self.format
+      self.options = data.options
     }
     ds.models.item.init(self, data)
 
@@ -28,8 +32,12 @@ ds.register_dashboard_item('summation_table', {
         data.format = self.format
       if (self.striped)
         data.striped = self.striped
+      if (self.show_color)
+        data.show_color = self.show_color
       if (self.title)
         data.title = self.title
+      if (self.options)
+        data.options = self.options
       return data
     }
 
@@ -37,10 +45,13 @@ ds.register_dashboard_item('summation_table', {
   },
 
   data_handler: function(query, item) {
+    var options = item.options || {}
+    var palette = ds.charts.util.get_palette(options.palette)
     var body = $('#' + item.item_id + ' tbody')
     body.empty()
-    query.data.forEach(function(series) {
-      body.append(ds.templates.models.summation_table_row({series:series, item:item}))
+    query.data.forEach(function(series, i) {
+      var color = palette[i % palette.length]
+      body.append(ds.templates.models.summation_table_row({series:series, item:item, color: color}))
     })
   },
 
@@ -48,7 +59,9 @@ ds.register_dashboard_item('summation_table', {
 
   interactive_properties: [
     { id: 'striped', type: 'boolean' },
+    { id: 'show_color', type: 'boolean' },
     'format',
     'title',
+    'chart.palette',
   ].concat(ds.models.item.interactive_properties)
 })
