9e831046d38d79058eedba9dfd4fe0a0a80f29be
aalpern <adam.alpern@gmail.com>
2015-02-28 13:02:20 -0800

    Update to bootstrap-datetimepicker 4.0.0

--- js/app/handlers/range-picker.js
+++ js/app/handlers/range-picker.js
@@ -1,6 +1,6 @@
 $(document).on('dp.change', '#ds-range-picker-from, #ds-range-picker-until', function(e) {
-  var from = $('#ds-range-picker-from').data("DateTimePicker").getDate()
-  var until = $('#ds-range-picker-until').data("DateTimePicker").getDate()
+  var from = $('#ds-range-picker-from').data("DateTimePicker").date()
+  var until = $('#ds-range-picker-until').data("DateTimePicker").date()
   if (from && until) {
     var GRAPHITE_FORMAT = 'HH:mm_YYYYMMDD'
     // console.log('setting range to ' + from.calendar() + ' until ' + until.calendar())
@@ -19,11 +19,11 @@ $(document).on('click', '.ds-recent-range-picker li, .ds-recent-range-picker a, 
     now.minute( Math.round(now.minute() / 15) * 15) // quantize to 15-min interval
 
     var from_picker = $('#ds-range-picker-from').data("DateTimePicker")
-    from_picker.setDate(now.clone().subtract('hours', 3))
+    from_picker.date(now.clone().subtract(3, 'hours'))
 
     var until_picker = $('#ds-range-picker-until').data("DateTimePicker")
-    until_picker.setDate(now)
-    until_picker.setMaxDate(now.clone().endOf('day'))
+    until_picker.date(now)
+    until_picker.maxDate(now.clone().endOf('day'))
 
   } else if (range === 'recent') {
     $('.ds-custom-range-picker').hide()
@@ -41,8 +41,8 @@ $(document).ready(function() {
     if (params.from && params.until) {
       // Initialise the range date pickers with the values from the URL query parameters
       var GRAPHITE_FORMAT = 'HH:mm_YYYYMMDD' // XXX This should a a scoped global in the ds object
-      $('#ds-range-picker-from').data("DateTimePicker").setDate(moment(params.from, GRAPHITE_FORMAT));
-      $('#ds-range-picker-until').data("DateTimePicker").setDate(moment(params.until, GRAPHITE_FORMAT));
+      $('#ds-range-picker-from').data("DateTimePicker").date(moment(params.from, GRAPHITE_FORMAT));
+      $('#ds-range-picker-until').data("DateTimePicker").date(moment(params.until, GRAPHITE_FORMAT));
       $('.ds-recent-range-picker').hide()
       $('.ds-custom-range-picker').show()
     }
