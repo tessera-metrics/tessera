f86ff9d59b0a98fcaa76fe24d8685aa603c19594
aalpern <adam.alpern@gmail.com>
2015-05-16 15:50:51 -0700

    Renamed time shift jumbotron consistently

--- src/ts/models/items/timeshift_jumbotron_singlestat.ts
+++ src/ts/models/items/timeshift_jumbotron_singlestat.ts
@@ -3,9 +3,9 @@ import { register_dashboard_item } from './factory'
 
 declare var ts
 
-export default class JumbotronTimeshiftSinglestat extends TimeshiftSinglestat {
+export default class TimeshiftJumbotronSinglestat extends TimeshiftSinglestat {
   static meta = {
     template: ts.templates.models.jumbotron_singlestat
   }
 }
-register_dashboard_item(JumbotronTimeshiftSinglestat)
+register_dashboard_item(TimeshiftJumbotronSinglestat)
