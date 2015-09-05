ab9833863882d2798c0257a37ef48a2585fde6eb
aalpern <adam.alpern@gmail.com>
2015-09-04 17:16:57 -0700

    Implemented translation

--- tessera-frontend/src/ts/importer/graphite.ts
+++ tessera-frontend/src/ts/importer/graphite.ts
@@ -1,18 +1,76 @@
 import { manager } from '../app'
 import * as models from '../models'
 import { logger } from '../core'
+import * as charts from '../charts'
 
-declare var URL
+declare var URL, inflection
 
 const log = logger('importer.graphite')
 
-function translate(dash: any) : models.Dashboard {
-  let columns = 3
-  let span    = 12 / columns
+function translate(gdash: any) : models.Dashboard {
+  return manager.without_updates(() => {
+    let columns = 3
+    let span = 12 / columns
 
-  console.log(dash)
+  let section = new models.Section()
+  let definition = new models.DashboardDefinition({
+    items: [section]
+  })
 
-  return new models.Dashboard({ title: 'test' })
+  let dash = new models.Dashboard({
+    title: inflection.dasherize(gdash.name),
+    category: 'Graphite',
+    tags: ['imported'],
+    definition: definition
+  })
+
+  let row = new models.Row()
+  for (let graph of gdash.graphs) {
+    let [targets, options, render_url] = graph
+    let is_stacked = render_url.indexOf('stacked') > -1 || (options.areaMode && options.areaMode === 'stacked')
+    let query_name = 'q' + Object.keys(definition.queries).length
+    let query = new models.Query({
+      name: query_name,
+      targets: options.target || []
+    })
+    definition.add_query(query)
+
+    let presentation = new models.StackedAreaChart({
+      query: query,
+      stack_mode: is_stacked ? charts.StackMode.NORMAL : charts.StackMode.NONE,
+      options: {
+        y1: {
+          label: options.vtitle,
+          max: options.yMax,
+          min: options.yMin
+        }
+      }
+    })
+
+    if (options.template) {
+      presentation.options.palette = options.template
+    }
+    if (options.vtitle) {
+      presentation.options.y1.label = options.vtitle
+    }
+
+    row.add(new models.Cell({
+      span: span,
+      items: [presentation]
+    }))
+
+    if (row.items.length === columns) {
+      section.add(row)
+      row = new models.Row()
+    }
+  }
+
+  if (row.items && row.items.length) {
+    section.items.push(row)
+  }
+
+  return dash
+  })
 }
 
 export function import_dashboard(url: string) : Promise<any> {
