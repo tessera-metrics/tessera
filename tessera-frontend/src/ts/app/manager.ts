5b7545dd7f5067292be16bfaa5f60f60c505be8f
aalpern <adam.alpern@gmail.com>
2015-10-23 15:46:30 -0700

    Add ts.manager.cleanup()

--- tessera-frontend/src/ts/app/manager.ts
+++ tessera-frontend/src/ts/app/manager.ts
@@ -163,15 +163,19 @@ export class Manager {
     return context
   }
 
+  cleanup() : void {
+    if (this.current && this.current.dashboard) {
+      this.current.dashboard.visit(item => item.cleanup())
+    }
+  }
+
   /**
    * Load and render a dashboard.
    */
   load(url: string, element, options: any = {}) : Manager {
     log.debug('load(): ' + url)
 
-    if (this.current && this.current.dashboard) {
-      this.current.dashboard.visit(item => item.cleanup())
-    }
+    this.cleanup()
     let holder = new DashboardHolder(url, element)
     let context = this._prep_url(url, options)
     this.set_current(holder)
