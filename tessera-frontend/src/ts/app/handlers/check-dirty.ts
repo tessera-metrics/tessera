4b8de73c3055b114f1195e12326cd9b6fef89a88
Adam Alpern <adam.alpern@gmail.com>
2016-12-10 09:40:12 -0800

    Check that the dashboard is set in window.onbeforeunload

--- tessera-frontend/src/ts/app/handlers/check-dirty.ts
+++ tessera-frontend/src/ts/app/handlers/check-dirty.ts
@@ -1,7 +1,7 @@
 declare var ts
 
 window.onbeforeunload = (e) => {
-  if (ts.manager.current.dashboard.dirty) {
+  if (ts.manager.current && ts.manager.current.dashboard.dirty) {
     let msg = 'Dashboard has unsaved changes. Are you sure you want to leave?'
     e.returnValue = msg
     return msg
