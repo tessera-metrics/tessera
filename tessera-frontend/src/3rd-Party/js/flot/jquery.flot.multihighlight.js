6829edd2396705946af200928fc224fdbce7f3d4
Adam Alpern <adam.alpern@gmail.com>
2019-07-22 08:47:11 -0700

    Issues #546: Reformat multihighlight plugin

--- tessera-frontend/src/3rd-Party/js/flot/jquery.flot.multihighlight.js
+++ tessera-frontend/src/3rd-Party/js/flot/jquery.flot.multihighlight.js
@@ -14,78 +14,79 @@ $(function() {
 			var plotsToHighlight = [ plot ];
 			if (plot.getOptions().multihighlight.linkedPlots != null) {
 				$.each(plot.getOptions().multihighlight.linkedPlots, function(index, linkedPlot){
-					plotsToHighlight.push(linkedPlot);	
+				  plotsToHighlight.push(linkedPlot);	
 				});
 			}
-			return plotsToHighlight;
+		  return plotsToHighlight;
 		}
-		
-		function onPlotHover(event, position, item) {
-			$.each(getPlotsToHighlight(), function(index, plotToHighlight) {
-				if (!plotToHighlight.getOptions().multihighlight.mode != null) {
-					plotToHighlight.unhighlight();
+	  
+	  function onPlotHover(event, position, item) {
+            
+	    $.each(getPlotsToHighlight(), function(index, plotToHighlight) {
+	      if (!plotToHighlight.getOptions().multihighlight.mode != null) {
+		plotToHighlight.unhighlight();
 
-					var axisPosition = plotToHighlight.getOptions().multihighlight.mode == 'x' ? position.x : position.y;
-					var dataIndex = plotToHighlight.getOptions().multihighlight.mode == 'x' ? 0 : 1;
+		var axisPosition = plotToHighlight.getOptions().multihighlight.mode == 'x' ? position.x : position.y;
+		var dataIndex = plotToHighlight.getOptions().multihighlight.mode == 'x' ? 0 : 1;
 
-					var highlightedItems = new Array();
-					$.each(plotToHighlight.getData(), function(i, serie) {
-						var j;
-						for (j = 0; j < serie.data.length; j++) {
-							if (serie.data[j] == null || serie.data[j][dataIndex] > axisPosition) {
-								break;
-							}
-						}
+		var highlightedItems = new Array();
+		$.each(plotToHighlight.getData(), function(i, serie) {
+		  var j;
+		  for (j = 0; j < serie.data.length; j++) {
+		    if (serie.data[j] == null || serie.data[j][dataIndex] > axisPosition) {
+		      break;
+		    }
+		  }
 
-						if (j != 0 && serie.data[j] !=null) {
-							var highlighted = j-1;
-							// Checking which one is closer.
-							if (axisPosition-serie.data[j-1][dataIndex] > Math.abs(axisPosition-serie.data[j][dataIndex])) {
-								highlighted = j;
-							}
+		  if (j != 0 && serie.data[j] !=null) {
+		    var highlighted = j-1;
+		    // Checking which one is closer.
+		    if (axisPosition-serie.data[j-1][dataIndex] > Math.abs(axisPosition-serie.data[j][dataIndex])) {
+		      highlighted = j;
+		    }
 
-							plotToHighlight.highlight(i, highlighted);
-							highlightedItems.push({ serieIndex: i, dataIndex: highlighted });
-						}
-					});
-					
-					if (highlightedItems.length > 0) {
-						plotToHighlight.getPlaceholder().trigger("multihighlighted", [ position, highlightedItems ]);		
-					}
-					else {
-						plotToHighlight.getPlaceholder().trigger("unmultihighlighted");
-					}
-				}
-			});
-		}
-		
-		function onMouseOut(event) {
-			$.each(getPlotsToHighlight(), function(index, plotToHighlight) {
-				if (!plotToHighlight.getOptions().multihighlight.mode != null) {
-					plotToHighlight.unhighlight();
-					plotToHighlight.getPlaceholder().trigger("unmultihighlighted");
-				}
-			});
-		}
-		
-		// Hook up the events.
-		plot.hooks.bindEvents.push(function(plot, eventHolder) {
-			if (!plot.getOptions().multihighlight.mode)
-				return;
-			
-			plot.getPlaceholder().bind('plothover', onPlotHover);
-			plot.getPlaceholder().bind('mouseout', onMouseOut);
+		    plotToHighlight.highlight(i, highlighted);
+		    highlightedItems.push({ serieIndex: i, dataIndex: highlighted });
+		  }
 		});
-		plot.hooks.shutdown.push(function(plot, eventHolder) {
-			plot.getPlaceholder().unbind('plothover', onPlotHover);
-			plot.getPlaceholder().unbind('mouseout', onMouseOut);
-		});
+		
+		if (highlightedItems.length > 0) {
+		  plotToHighlight.getPlaceholder().trigger("multihighlighted", [ position, highlightedItems ]);		
+		}
+		else {
+		  plotToHighlight.getPlaceholder().trigger("unmultihighlighted");
+		}
+	      }
+	    });
+	  }
+	  
+	  function onMouseOut(event) {
+	    $.each(getPlotsToHighlight(), function(index, plotToHighlight) {
+	      if (!plotToHighlight.getOptions().multihighlight.mode != null) {
+		plotToHighlight.unhighlight();
+		plotToHighlight.getPlaceholder().trigger("unmultihighlighted");
+	      }
+	    });
+	  }
+	  
+	  // Hook up the events.
+	  plot.hooks.bindEvents.push(function(plot, eventHolder) {
+	    if (!plot.getOptions().multihighlight.mode)
+	      return;
+	    
+	    plot.getPlaceholder().bind('plothover', onPlotHover);
+	    plot.getPlaceholder().bind('mouseout', onMouseOut);
+	  });
+	  plot.hooks.shutdown.push(function(plot, eventHolder) {
+	    plot.getPlaceholder().unbind('plothover', onPlotHover);
+	    plot.getPlaceholder().unbind('mouseout', onMouseOut);
+	  });
 	}
-	
-	$.plot.plugins.push({
-		init: init,
-		options: options,
-		name: 'multihighlight',
-		version: '1.0'
-	});
+  
+  $.plot.plugins.push({
+    init: init,
+    options: options,
+    name: 'multihighlight',
+    version: '1.0'
+  });
 });
